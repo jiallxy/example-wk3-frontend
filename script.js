1f7296835172e3c4793518a78050dc7dd4200952
Justin Luo <87254766+jiallxy@users.noreply.github.com>
2025-10-22 19:43:13 -0400

    2

---
+++ script.js
@@ -0,0 +1,30 @@
+//
+// JavaScript adds interactivity and dynamic content.
+// This includes interacting with the back-end code.
+//
+
+// 1. Get references to the HTML elements we need to interact with.
+const button = document.getElementById('actionButton');
+const greetingText = document.getElementById('greeting');
+const myList=document.getElementById('myList');
+
+// 2. "Listen" for the click event on the button and run the function.
+// This connects the user action (click) to the code logic (handleClick function).
+button.addEventListener('click', handleClick);
+
+// 3. Define a function that will be executed when the button is clicked.
+// Note that this function calls the back-end code
+// Note the use of: async/await keywords
+async function handleClick() {
+
+    // set back-end code url - CHANGE IT TO YOUR OWN URL!
+    const backendUrl = "https://example-wk3-backend-umber.vercel.app";  // IMPORTANT: no slash at the end
+
+    // fetch the data by calling 'backendUrl/route' (note the slash before the route)
+    let resp = await fetch(`${backendUrl}/example`);           
+    let data = await resp.json();
+
+    // do something with the data    
+    greetingText.textContent = data.message;    
+    
+}
